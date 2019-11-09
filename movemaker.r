feb1efe6a3c8182a19c72e5e78b3a4fd9a1a77b6
Isaac Adjei-Attah <adjei-attahisaac@gmail.com>
2019-11-09 10:07:09 +0000

    Added first file

---
+++ movemaker.r
@@ -0,0 +1,16 @@
+##################################################################################################
+# Author: Isaac Adjei-Attah
+# MoveMaker Algorithm
+# Objective: 
+##  To develop and effective Miniload re-organisation model based on anticipated shop order demand
+##################################################################################################
+##################################################################################################
+
+# Begin with all stocked products in the Miniload
+
+
+# Assign likelihood of order and anticipated units
+
+
+# Assign likelihood of tote selection for totes containing chosen products, and meeting units criteria
+
