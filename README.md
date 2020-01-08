d70a6464da4b59fd2718e437eb99f9e78a6dfeae
Isaac Adjei-Attah <isaac.adjei-attah@johnlewis.co.uk>
2020-01-08 16:06:36 +0000

    First complete algorithm for MoveMaker

--- README.md
+++ README.md
@@ -7,9 +7,21 @@
 
 ## Begin with all stocked products in the Miniload
 
-
-## Assign likelihood of order and anticipated units
-
+## Approach
+As this algorithm relies on shop demand for stocked products, so best approach may be to develop a 
+product-level forecast.
 
 ## Assign likelihood of tote selection for totes containing chosen products, and meeting units criteria
+
+## Decision Rule:
+# A. For each product in ASRS, sort totes in ascending order of units
+
+
+# 1. Products in OSR whose units < Expected Units
+#   a. Move required number of totes where total stock >= required difference fro ASRS to OSR
+
+
+# 2. Products in OSR whose units >= Expected Units
+#   a. Send extra totes to ASRS, where necessary
+#   b. Do nothing where OSR Units = Expected Units
 
--- movemaker.r
+++ movemaker.r
@@ -13,58 +13,136 @@ gc()
 ################################################################################
 
 ##########################################################################################################
-simcon <- dbConnect(odbc::odbc(), dsn = "simulation", pwd = "sim123")
+livecon <- dbConnect(odbc::odbc(), dsn = "live", pwd = "atis0815")
 ##########################################################################################################
 
-average_sales <- tbl(simcon, sql(
+stock <- tbl(livecon, sql(
 "
-with base 
-as
+select location
+  , locno
+  , z_pos
+  , loccount
+  , licenceplate
+  , item_number
+  , quantity
+  , sum(quantity) over (partition by item_number, location order by quantity desc, loccount desc, locno, z_pos, licenceplate)cumunits
+  , row_number() over (partition by item_number, location order by quantity desc, loccount desc, locno, z_pos, licenceplate)itemrank
+from
 (
-  select trunc(orh.creation_time)orderdate
-    , p.system_product_id prod_id
-    , p.product_no prod_code
-    , p.item_desc description
-    , sum(orl.requested_quantity)units
-    , count(distinct orh.order_no)orders
-  
-  from (
-    select * from originator_headers@lwms
-    union
-    select * from k1233kw_dw.originator_headers@lwms
-  )orh
-    inner join
+  select location
+    , licenceplate
+    , item_number
+    , quantity
+    , count(*) over (partition by item_number, locno, location order by locno)loccount
+    , locno
+    , z_pos
+  from
   (
-    select * from originator_lines@lwms
-    union
-    select * from k1233kw_dw.originator_lines@lwms
-  )orl on orh.id = orl.originator_header_id
-    inner join products@lwms p on orl.item_id = p.id
-  where trunc(orh.creation_time) > trunc(sysdate) - 30
-    and orh.order_type = 0
-  group by trunc(orh.creation_time)
-    , p.system_product_id
-    , p.product_no
-    , p.item_desc
+    select case
+        when substr(locno, 1, 3) ='OSM' then 'OSR'
+        when substr(locno, 1, 3) = 'ASM' then 'ASRS'
+      end location
+      , licenceplate
+      , item_number
+      , quantity
+      , locno
+      , z_pos
+    from v_afm_stock@motion
+    where substr(locno, 1, 3) in ('OSM', 'ASM')
+  )
 )
-select prod_id
-  , prod_code
-  , description
-  , avg(units)avg_units
-  , avg(orders)avg_orders
-  , count(*)days_ordered
-from base
-group by prod_id
-  , prod_code
-  , description
 "
 )) %>%
   collect() %>%
   setDT(.)
 
-dbDisconnect(simcon)
+
+average_sales <- tbl(livecon, sql("select * from average_daily_sales@k1233simu")) %>%
+  collect() %>%
+  setDT(.)
+
+dbDisconnect(livecon)
+
 
 ###############################################################################################################
 
-average_sales[, LIKELIHOOD := DAYS_ORDERED /30][, ':='(EXP_UNITS = round(LIKELIHOOD * AVG_UNITS), EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS))]
+asrs <- merge(stock[LOCATION == 'ASRS'], average_sales, by.x = "ITEM_NUMBER", by.y = "PROD_ID")[, TOTAL_STOCK := sum(QUANTITY), by=ITEM_NUMBER]
+osr <- merge(stock[LOCATION == 'OSR'], average_sales, by.x = "ITEM_NUMBER", by.y = "PROD_ID")[, TOTAL_STOCK := sum(QUANTITY), by=ITEM_NUMBER]
 
+
+
+# Begin with products in the OSR
+##############################################################################################################
+
+to_asrs <- osr %>%
+  mutate(LIKELIHOOD = DAYS_ORDERED / 30,
+         EXP_UNITS = round(LIKELIHOOD * AVG_UNITS),
+         EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS),
+         CLASSIFICATION = if_else(EXP_UNITS <= CUMUNITS, 1, 0)
+         ) %>%
+  filter(LIKELIHOOD >= 0.5) %>%
+  filter(TOTAL_STOCK >= EXP_UNITS) %>%
+  group_by(ITEM_NUMBER, CLASSIFICATION) %>%
+  mutate(RNK = 1:n()) %>%
+  filter(CLASSIFICATION == 1 & RNK > 1) %>%
+  ungroup() %>%
+  select(ITEM_NUMBER, LOCATION, LOCNO, LICENCEPLATE, QUANTITY) %>%
+  setDT(.)
+
+
+## Determine Shortfall in OSR
+
+osr_shortfall <- osr %>%
+  mutate(LIKELIHOOD = DAYS_ORDERED / 30,
+         EXP_UNITS = round(LIKELIHOOD * AVG_UNITS),
+         EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS),
+         CLASSIFICATION = if_else(EXP_UNITS <= CUMUNITS, 1, 0)
+  ) %>%
+  filter(LIKELIHOOD >= 0.5) %>%
+  filter(TOTAL_STOCK < EXP_UNITS) %>%
+  group_by(ITEM_NUMBER) %>%
+  summarise(REQUIRED = min(EXP_UNITS - TOTAL_STOCK)) %>%
+  setDT(.)
+
+
+
+to_osr1 <- asrs %>%
+  inner_join(osr_shortfall, by="ITEM_NUMBER") %>%
+  mutate(LIKELIHOOD = DAYS_ORDERED / 30,
+         EXP_UNITS = round(LIKELIHOOD * AVG_UNITS),
+         EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS),
+         CLASSIFICATION = if_else(REQUIRED <= CUMUNITS, 1, 0)
+  ) %>%
+  filter(LIKELIHOOD >= 0.5) %>%
+  group_by(ITEM_NUMBER, CLASSIFICATION) %>%
+  mutate(RNK = 1:n()) %>%
+  filter(CLASSIFICATION == 0 | CLASSIFICATION == 1 & RNK == 1) %>%
+  ungroup() %>%
+  select(ITEM_NUMBER, LOCATION, LOCNO, LICENCEPLATE, QUANTITY) %>%
+  setDT(.)
+
+
+
+# Products in ASRS not in OSR, with expected sales
+
+to_osr2 <- asrs %>%
+  inner_join(data.frame(ITEM_NUMBER = setdiff(asrs$ITEM_NUMBER, osr$ITEM_NUMBER), stringsAsFactors = F), by="ITEM_NUMBER") %>%
+  mutate(LIKELIHOOD = DAYS_ORDERED / 30,
+         EXP_UNITS = round(LIKELIHOOD * AVG_UNITS),
+         EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS),
+         CLASSIFICATION = if_else(EXP_UNITS <= CUMUNITS, 1, 0)
+  ) %>%
+  filter(LIKELIHOOD >= 0.5) %>%
+  group_by(ITEM_NUMBER, CLASSIFICATION) %>%
+  mutate(RNK = 1:n()) %>%
+  filter(CLASSIFICATION == 0 | CLASSIFICATION == 1 & RNK == 1) %>%
+  ungroup() %>%
+  select(ITEM_NUMBER, LOCATION, LOCNO, LICENCEPLATE, QUANTITY) %>%
+  setDT(.)
+
+
+to_osr <- to_osr1 %>%
+  rbind(to_osr2)
+
+
+moves <- rbind(cbind(DIRECTION = "TO_ASRS", to_asrs), cbind(DIRECTION = "TO_OSR", rbind(to_osr1, to_osr2)))
