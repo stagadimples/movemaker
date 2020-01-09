7a859d5e1d071b35777a4d12bb31117eb06e97bf
Isaac Adjei-Attah <isaac.adjei-attah@johnlewis.co.uk>
2020-01-09 13:12:34 +0000

    Made major revisions to the code for efficiency and speed

--- movemaker.r
+++ movemaker.r
@@ -38,17 +38,24 @@ from
     , z_pos
   from
   (
-    select case
-        when substr(locno, 1, 3) ='OSM' then 'OSR'
-        when substr(locno, 1, 3) = 'ASM' then 'ASRS'
-      end location
-      , licenceplate
-      , item_number
-      , quantity
-      , locno
-      , z_pos
-    from v_afm_stock@motion
-    where substr(locno, 1, 3) in ('OSM', 'ASM')
+   select case
+      when substr(l.locno, 1, 3) ='OSM' then 'OSR'
+      when substr(l.locno, 1, 3) = 'ASM' then 'ASRS'
+    end location
+    , c.licenceplate
+    , i.item_number
+    , st.act_quantity quantity
+    , l.locno
+    , c.z_pos
+   from stock@motion st
+    inner join cont@motion c on st.cont_id = c.id
+    inner join item@motion i on st.item_id = i.id
+    inner join loc@motion l on c.loc_id = l.id
+    inner join tm_locs@motion tl on c.loc_id = tl.loc_id
+    inner join states@motion s on tl.state_id = s.id
+   where c.cont_type_id != 0
+    and substr(l.locno, 1, 3) in ('OSM', 'ASM')
+    and s.domain_id = 2
   )
 )
 "
@@ -57,8 +64,14 @@ from
   setDT(.)
 
 
-average_sales <- tbl(livecon, sql("select * from average_daily_sales@k1233simu")) %>%
+daily_sales <- tbl(livecon, sql(
+"
+select * from average_daily_sales@k1233simu
+where order_type = 'SHOP'
+"
+)) %>%
   collect() %>%
+  filter(LIKELIHOOD >= 0.5) %>% # specify acceptable likelihood
   setDT(.)
 
 dbDisconnect(livecon)
@@ -66,8 +79,8 @@ dbDisconnect(livecon)
 
 ###############################################################################################################
 
-asrs <- merge(stock[LOCATION == 'ASRS'], average_sales, by.x = "ITEM_NUMBER", by.y = "PROD_ID")[, TOTAL_STOCK := sum(QUANTITY), by=ITEM_NUMBER]
-osr <- merge(stock[LOCATION == 'OSR'], average_sales, by.x = "ITEM_NUMBER", by.y = "PROD_ID")[, TOTAL_STOCK := sum(QUANTITY), by=ITEM_NUMBER]
+asrs <- merge(stock[LOCATION == 'ASRS'], daily_sales, by.x = "ITEM_NUMBER", by.y = "PROD_ID")[, TOTAL_STOCK := sum(QUANTITY), by=ITEM_NUMBER]
+osr <- merge(stock[LOCATION == 'OSR'], daily_sales, by.x = "ITEM_NUMBER", by.y = "PROD_ID")[, TOTAL_STOCK := sum(QUANTITY), by=ITEM_NUMBER]
 
 
 
@@ -75,13 +88,8 @@ osr <- merge(stock[LOCATION == 'OSR'], average_sales, by.x = "ITEM_NUMBER", by.y
 ##############################################################################################################
 
 to_asrs <- osr %>%
-  mutate(LIKELIHOOD = DAYS_ORDERED / 30,
-         EXP_UNITS = round(LIKELIHOOD * AVG_UNITS),
-         EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS),
-         CLASSIFICATION = if_else(EXP_UNITS <= CUMUNITS, 1, 0)
-         ) %>%
-  filter(LIKELIHOOD >= 0.5) %>%
-  filter(TOTAL_STOCK >= EXP_UNITS) %>%
+  mutate(CLASSIFICATION = if_else(EXP_DEMAND <= CUMUNITS, 1, 0)) %>%
+  filter(TOTAL_STOCK >= EXP_DEMAND) %>%
   group_by(ITEM_NUMBER, CLASSIFICATION) %>%
   mutate(RNK = 1:n()) %>%
   filter(CLASSIFICATION == 1 & RNK > 1) %>%
@@ -93,27 +101,17 @@ to_asrs <- osr %>%
 ## Determine Shortfall in OSR
 
 osr_shortfall <- osr %>%
-  mutate(LIKELIHOOD = DAYS_ORDERED / 30,
-         EXP_UNITS = round(LIKELIHOOD * AVG_UNITS),
-         EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS),
-         CLASSIFICATION = if_else(EXP_UNITS <= CUMUNITS, 1, 0)
-  ) %>%
-  filter(LIKELIHOOD >= 0.5) %>%
-  filter(TOTAL_STOCK < EXP_UNITS) %>%
+  mutate(CLASSIFICATION = if_else(EXP_DEMAND <= CUMUNITS, 1, 0)) %>%
+  filter(TOTAL_STOCK < EXP_DEMAND) %>%
   group_by(ITEM_NUMBER) %>%
-  summarise(REQUIRED = min(EXP_UNITS - TOTAL_STOCK)) %>%
+  summarise(REQUIRED = min(EXP_DEMAND - TOTAL_STOCK)) %>%
   setDT(.)
 
 
 
 to_osr1 <- asrs %>%
   inner_join(osr_shortfall, by="ITEM_NUMBER") %>%
-  mutate(LIKELIHOOD = DAYS_ORDERED / 30,
-         EXP_UNITS = round(LIKELIHOOD * AVG_UNITS),
-         EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS),
-         CLASSIFICATION = if_else(REQUIRED <= CUMUNITS, 1, 0)
-  ) %>%
-  filter(LIKELIHOOD >= 0.5) %>%
+  mutate(CLASSIFICATION = if_else(REQUIRED <= CUMUNITS, 1, 0)) %>%
   group_by(ITEM_NUMBER, CLASSIFICATION) %>%
   mutate(RNK = 1:n()) %>%
   filter(CLASSIFICATION == 0 | CLASSIFICATION == 1 & RNK == 1) %>%
@@ -122,17 +120,11 @@ to_osr1 <- asrs %>%
   setDT(.)
 
 
-
 # Products in ASRS not in OSR, with expected sales
 
 to_osr2 <- asrs %>%
   inner_join(data.frame(ITEM_NUMBER = setdiff(asrs$ITEM_NUMBER, osr$ITEM_NUMBER), stringsAsFactors = F), by="ITEM_NUMBER") %>%
-  mutate(LIKELIHOOD = DAYS_ORDERED / 30,
-         EXP_UNITS = round(LIKELIHOOD * AVG_UNITS),
-         EXP_ORDERS = round(LIKELIHOOD * AVG_ORDERS),
-         CLASSIFICATION = if_else(EXP_UNITS <= CUMUNITS, 1, 0)
-  ) %>%
-  filter(LIKELIHOOD >= 0.5) %>%
+  mutate(CLASSIFICATION = if_else(EXP_DEMAND <= CUMUNITS, 1, 0)) %>%
   group_by(ITEM_NUMBER, CLASSIFICATION) %>%
   mutate(RNK = 1:n()) %>%
   filter(CLASSIFICATION == 0 | CLASSIFICATION == 1 & RNK == 1) %>%
@@ -145,4 +137,6 @@ to_osr <- to_osr1 %>%
   rbind(to_osr2)
 
 
-moves <- rbind(cbind(DIRECTION = "TO_ASRS", to_asrs), cbind(DIRECTION = "TO_OSR", rbind(to_osr1, to_osr2)))
+# Prescribed moves based on expected shop demand for the day
+moves <- rbind(cbind(TARGET = "ASRS", to_asrs), cbind(TARGET = "OSR", rbind(to_osr1, to_osr2)))[
+  , .(LOCATION, TARGET, ITEM_NUMBER, LOCNO, LICENCEPLATE, QUANTITY)]
